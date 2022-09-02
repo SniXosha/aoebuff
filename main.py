186069769606eaf012f29f5e9082656bf3857381
apavlov <artem.pavlov.yu@gmail.com>
2022-09-02 22:26:36 +0300

    Save to file instead of global variable

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
-match_history = []
+import json
+import os.path
+
+data_path = 'data/matches.json'
 
 
-def add_game(nickname, race, result):
-    global match_history
+def add_game(matches, nickname, race, result):
     game = {
         'nickname': nickname,
         'race': race,
         'result': result
     }
-    match_history.append(game)
+    matches.append(game)
 
 
-def list_games():
-    for game in match_history:
+def list_games(matches):
+    for game in matches:
         print(game.get('nickname') + '(' + game.get('race') + ')' + ' - ' + game.get('result'))
 
 
+def load_history(path):
+    if not os.path.exists(path):
+        return []
+
+    with open(path, 'r') as file:
+        return json.load(file)
+
+
+def save_history(history, path):
+    with open(path, 'w') as file:
+        return json.dump(history, file)
+
+
 if __name__ == '__main__':
-    add_game('Mista', 'eng', 'win')
-    add_game('Beastyqt', 'abbasid', 'loss')
-    list_games()
+    match_history = load_history(data_path)
+
+    add_game(match_history, 'Mista', 'eng', 'win')
+    add_game(match_history, 'Beastyqt', 'abbasid', 'loss')
+    list_games(match_history)
+
+    save_history(match_history, data_path)
