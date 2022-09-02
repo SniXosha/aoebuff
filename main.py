9509dbc9cf15ad47da36a598973ddf5318e6cad7
apavlov <artem.pavlov.yu@gmail.com>
2022-09-02 21:44:28 +0300

    Implemented initial match history functionality

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+match_history = []
+
+
+def add_game(nickname, race, result):
+    global match_history
+    game = {
+        'nickname': nickname,
+        'race': race,
+        'result': result
+    }
+    match_history.append(game)
+
+
+def list_games():
+    for game in match_history:
+        print(game.get('nickname') + '(' + game.get('race') + ')' + ' - ' + game.get('result'))
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    add_game('Mista', 'eng', 'win')
+    add_game('Beastyqt', 'abbasid', 'loss')
+    list_games()
