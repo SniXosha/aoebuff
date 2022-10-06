c3b5233329eddddf76b053b115262b9a171fc92c
DFerar <mr.ferar@gmail.com>
2022-10-06 14:01:55 +0400

    Add count_games and count_winrate functions and same commands. Code for function request has been reworked

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from serialize import load_obj, save_obj
-from process_commands_test import process_commands_test
+from process_commands import process_commands
 
 matches_path = 'data/matches.json'
 
 if __name__ == '__main__':
     matches = load_obj(matches_path, [])
 
-    process_commands_test(matches)
+    process_commands(matches)
 
     save_obj(matches, matches_path)
--- process_commands.py
+++ process_commands.py
@@ -1,24 +1,34 @@
 from game import add_game, list_games
-from count_winrate import count_games, count_winrate
+from game_stats import count_games, count_winrate
+
+
+def str_to_dict(main_command_arguments):
+    dictionary = {}
+    for pairs in main_command_arguments:
+        pair = pairs.split('=')
+        dictionary[pair[0]] = pair[1]
+    return dictionary
 
 
 def process_commands(matches):
     while True:
-        first_command = input('Enter the command: ')
-        if first_command == 'ADD':
-            nickname = input('Nickname: ')
-            race = input('Race: ')
-            result = input('Result: ')
+        command_name = input('Enter the command: ')
+        command_tokens = command_name.split()
+        command_token_function = command_tokens[0]
+        command_tokens_arguments = command_tokens[1:]
+        arguments_dict = str_to_dict(command_tokens_arguments)
+        if command_token_function == 'ADD':
+            nickname = arguments_dict['nickname']
+            race = arguments_dict['race']
+            result = arguments_dict['result']
             add_game(matches, nickname, race, result)
-        elif first_command == 'LIST_OF_GAMES':
+        elif command_token_function == 'LIST_OF_GAMES':
             list_games(matches)
-        elif first_command == 'STOP':
+        elif command_token_function == 'STOP':
             break
-        elif first_command == 'COUNT':
-            nickname_for_count = input('Enter the nickname of player: ')
+        elif command_token_function == 'COUNT':
+            nickname_for_count = arguments_dict['nickname']
             print(count_games(matches, nickname_for_count))
-        elif first_command == 'WIN_RATE':
-            nickname_for_count_wins = input('Enter the nickname of player: ')
-            print(str(count_winrate(matches, nickname_for_count_wins)) + '%')
-
-
+        elif command_token_function == 'WIN_RATE':
+            nickname_for_count_wins = arguments_dict['nickname']
+            print(count_winrate(matches, nickname_for_count_wins))
